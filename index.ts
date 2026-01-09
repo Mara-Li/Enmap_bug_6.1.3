b876de69179491b52ee5c00ed5ad40ee77e57907
Mara-Li <lili.simonetti@outlook.fr>
2026-01-09 10:25:34 +0100

    feat: add MyServerData interface and integrate Enmap for server data management

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-interface MyServerData = {
+import Enmap from "enmap";
 
+interface MyServerData {
+	language: string;
+	welcomeMessage: string;
 }
+
+const database: Enmap<string, MyServerData> = new Enmap({
+	name: "myServerData",
+});
+
+database.set("server123", {
+	language: "en",
+	welcomeMessage: "Welcome to the server!",
+})
+
+database.set("server123", "fr", "language");
+//TS2345: Argument of type "language" is not assignable to parameter of type
+// "length" | "toString" | "concat" | "slice" | "indexOf" | "lastIndexOf" | "includes" | "at" | "charAt" | "charCodeAt" | "localeCompare" | "match" | "replace" | "search" | "split" | ... 36 more ... | undefined
+
+const key = database.get("server123", "language");
+//Results with latest typescript & enmap version:
+//TS2345: Argument of type "language" is not assignable to parameter of type
+// "length" | "toString" | "concat" | "slice" | "indexOf" | "lastIndexOf" | "includes" | "at" | "charAt" | "charCodeAt" | "localeCompare" | "match" | "replace" | "search" | "split" | ... 36 more ... | undefined
